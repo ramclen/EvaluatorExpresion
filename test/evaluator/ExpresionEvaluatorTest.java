aef09d37a7cce60dcdb9e6e2c35f109f7abe8f55
usuario <usuario@10.22.147.135>
2014-02-10 13:21:18 +0000

    ArithmeticExpressionTest passed

---
+++ test/evaluator/ExpresionEvaluatorTest.java
@@ -0,0 +1,16 @@
+package evaluator;
+
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+
+public class ExpresionEvaluatorTest {
+    
+    @Test
+    public void arithmeticExpressionTest(){
+        ExpresionEvaluator evaluator = new ExpresionEvaluator();
+        Assert.assertEquals(7, evaluator.execute(1+2*3)); 
+    }
+    
+}
