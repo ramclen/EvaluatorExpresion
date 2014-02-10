cd8e8004ea44d53b22aa1d4a5934bca4b84ef8e1
Cristopher López Santana <cristopher.correo@gmail.com>
2014-02-10 19:22:22 +0000

    ConstantTest Created

--- test/evaluator/ExpresionEvaluatorTest.java
+++ test/evaluator/ExpresionEvaluatorTest.java
@@ -1,5 +1,11 @@
 package evaluator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ExpresionEvaluatorTest {
-    
+    @Test
+    public void constantTest(){
+        Assert.assertEquals(1, new Number("1").getResult());
+    }
 }
