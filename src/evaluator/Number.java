f45e8a525c275c6082ef7c30c6dfb7909e4b1799
Cristopher López Santana <cristopher.correo@gmail.com>
2014-02-10 19:25:12 +0000

    NumberClass Created

---
+++ src/evaluator/Number.java
@@ -0,0 +1,12 @@
+package evaluator;
+
+public class Number {
+    private String representation;
+
+    Number(String representation) {
+        this.representation = representation;
+    }
+    public int getResult(){
+        return 1;
+    }
+}
