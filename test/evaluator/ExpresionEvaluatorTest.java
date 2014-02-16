63e366da0be94fe94a15cdc31d01ca3a279bafab
Cristopher López Santana <cristopher.correo@gmail.com>
2014-02-16 13:10:16 +0000

    LessThanTest created

--- test/evaluator/ExpresionEvaluatorTest.java
+++ test/evaluator/ExpresionEvaluatorTest.java
@@ -7,27 +7,33 @@ public class ExpresionEvaluatorTest {
     
     @Test
     public void constantTest(){
-        Assert.assertEquals(1, new Number("1").getResult()); 
-        Assert.assertEquals(2, new Number("2").getResult()); 
+        Assert.assertEquals(1, new Constant(1).getResult().getValue()); 
+        Assert.assertEquals(2, new Constant(2).getResult().getValue()); 
     }
     
     @Test
     public void sumTest(){
-        Operation operation = new Operation("+", new Number("1"), new Number("2"));
-        Assert.assertEquals(3, operation.getResult());
+        Operation operation = new Operation("+", new Constant(1), new Constant(2));
+        Assert.assertEquals(3, operation.getResult().getValue());
         
-        operation = new Operation("+", new Number("5"), new Number("2"));
-        Assert.assertEquals(7, operation.getResult());
+        operation = new Operation("+", new Constant(5), new Constant(2));
+        Assert.assertEquals(7, operation.getResult().getValue());
     }
     
     @Test
     public void multiplicationAndSumTest(){
-        Operation mulOperation = new Operation("*", new Number("2"), new Number("3"));
-        Operation sumOperation = new Operation("+", new Number("1"), mulOperation);
-        Assert.assertEquals(7, sumOperation.getResult());
+        Operation mulOperation = new Operation("*", new Constant(2), new Constant(3));
+        Operation sumOperation = new Operation("+", new Constant(1), mulOperation);
+        Assert.assertEquals(7, sumOperation.getResult().getValue());
     }
     
     @Test
+    public void lessThanTest(){
+        Operation sumOperation = new Operation("<", new Constant(1), new Constant(3));
+        Assert.assertEquals(7, sumOperation.getResult().getValue());
+    }
+    
+    
     public void complexExpressionTest(){
         ExpressionEvaluator evaluator = new ExpressionEvaluator();
         Assert.assertEquals(7, evaluator.getOperation("1+2*3").getResult());
