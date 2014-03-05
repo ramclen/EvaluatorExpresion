3759eb99d14a4d95743b2ce9362bbd0bc2fcc062
Cristopher López Santana <cristopher.correo@gmail.com>
2014-03-05 19:56:09 +0000

    CREATED StringsParserTest

---
+++ test/evaluator/ParserTest.java
@@ -0,0 +1,27 @@
+package evaluator;
+
+import evaluator.parsers.ExpressionToken;
+import evaluator.parsers.StringsParser;
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+public class ParserTest {
+
+    public void tokenParserTest() {
+
+
+    }
+
+    @Test
+    public void StringsParserTest() {
+        Stack<ExpressionToken> stackResult = new StringsParser().parse("3+2");
+        Stack<ExpressionToken> stackExpected = new Stack<>();
+        stackExpected.push(new ConstantToken(3));
+        stackExpected.push(new OperatorToken(+));
+        stackExpected.push(new ConstantToken(2));
+        assertEquals(stackExpected, stackResult);
+    }
+}
