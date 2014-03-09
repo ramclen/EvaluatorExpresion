0197cc5b9f2a018179630ccfb1440cefb39fe947
Cristopher López Santana <cristopher.correo@gmail.com>
2014-03-09 21:48:20 +0000

    another solution reached

--- src/evaluator/parsers/tokenizer/ConstantTokenizer.java
+++ src/evaluator/parsers/tokenizer/ConstantTokenizer.java
@@ -16,40 +16,18 @@ public class ConstantTokenizer implements ExpressionTokenizer<ConstantToken>{
     }
 
     private void extractConstants(String string) {
-        List<Integer> positionList = getOperatorsPosition(string);
-        int firstIndex = 0;
-        for (Integer lastIndex : positionList) {
-            constants.add(string.substring(firstIndex, lastIndex));
-            firstIndex = lastIndex+1;
-        }
+        int lastIndex=0;
+        for (int index = 0; index < string.length(); index++)
+            if (isOperator(string, index)) {
+                constants.add(string.substring(lastIndex, index));
+                lastIndex = index+1;
+            }
     }
 
-    private List<Integer> getOperatorsPosition(String string) {
-        List<Integer> positions = new LinkedList<>();
-        findAndGetSymbolsPosition(string, positions);
-        return positions;
+    private boolean isOperator(String string, int index) {
+        return new OperatorDictionary().containsKey(String.valueOf(string.charAt(index)));
     }
 
-    private void findAndGetSymbolsPosition(String string, List<Integer> positions) {
-        for (String symbol : new OperatorDictionary().keySet())
-            if (string.contains(symbol))
-                positions.addAll(getSymbolPositions(string, symbol));
-        Collections.sort(positions);
-    }
-
-    private List<Integer> getSymbolPositions(String string, String symbol) {
-        Stack<Integer> positions = new Stack<>();
-        for (String slice : string.split("\\" + symbol))
-            putNewPosition(positions, slice);
-        return positions.subList(0,positions.size()-2);
-    }
-
-    private void putNewPosition(Stack<Integer> positions, String slice) {
-        if (positions.isEmpty())
-            positions.push(slice.length());
-        else
-            positions.push(positions.peek() + slice.length()+1);
-    }
 
     @Override
     public boolean hasMoreElements() {
